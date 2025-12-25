2bb3725cfbfa97d326eb0fbde41147f443d1d389
Vali Marin <valimarin01@gmail.com>
2025-12-25 12:09:52 +0100

    new update

--- script.js
+++ script.js
@@ -3,12 +3,37 @@ const generateButton = document.getElementById("generateButton");
 const nrButtons = document.getElementById("nrButtons");
 const message = document.getElementById("message");
 const buttons = [];
+let winningButton = 0;
 
-function generate() {
-  const winningButton = Math.floor(Math.random() * nrButtons.value) + 1;
-  generateButton.disabled = true;
-  message.textContent = "Find the hidden button!";
+function reset(time) {
+  setTimeout(() => {
+    generateButton.disabled = false;
+    nrButtons.value = "";
+    message.textContent = "Generate!";
+    buttons.forEach((button) => {
+      button.remove();
+    });
+    buttons.length = 0;
+  }, time);
+}
 
+function clickEvent(id) {
+  const idButton = document.getElementById(id);
+  idButton.disabled = true;
+
+  if (idButton.id == winningButton) {
+    idButton.textContent = "X";
+    idButton.classList.replace("btn-primary", "btn-success");
+    message.textContent = "Hidden button found!";
+    reset(3000);
+  } else {
+    idButton.textContent = "!";
+    idButton.classList.replace("btn-primary", "btn-danger");
+    message.textContent = "Wrong button!";
+  }
+}
+
+function createButton() {
   for (let i = 1; i <= nrButtons.value; ++i) {
     const button = document.createElement("button");
     button.textContent = "?";
@@ -18,28 +43,14 @@ function generate() {
     buttons.push(button);
 
     button.addEventListener("click", (e) => {
-      const idButton = document.getElementById(e.target.id);
-
-      if (idButton.id == winningButton) {
-        idButton.textContent = "X";
-        idButton.classList.replace("btn-primary", "btn-success");
-        message.textContent = "Hidden button found!";
-
-        setTimeout(() => {
-          generateButton.disabled = false;
-          nrButtons.value = "";
-          message.textContent = "Generate!";
-          buttons.forEach((button) => {
-            button.remove();
-          });
-          buttons.length = 0;
-        }, 3000);
-      } else {
-        idButton.textContent = "!";
-        idButton.classList.replace("btn-primary", "btn-danger");
-        message.textContent = "Wrong button!";
-      }
-      idButton.disabled = true;
+      clickEvent(e.target.id);
     });
   }
+}
+
+function generate() {
+  winningButton = Math.floor(Math.random() * nrButtons.value) + 1;
+  generateButton.disabled = true;
+  message.textContent = "Find the hidden button!";
+  createButton();
 }
